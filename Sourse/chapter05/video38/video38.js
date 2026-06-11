00d9a340a9e3589fa616dc1fcaf85530600eab9e
NguyenXuanAn07 <anxuan.ptit@gmail.com>
2026-06-11 15:11:49 +0700

    update

---
+++ Sourse/chapter05/video38/video38.js
@@ -0,0 +1,18 @@
+
+
+const names = ["Xuan AN", "heehehehehe"]
+console.log(names[0])
+names[2] = "ngoc minh"
+console.log(names)
+
+names.push(123)
+console.log(names)
+
+names.pop()
+console.log(names)
+
+names.unshift(null)
+console.log(names)
+
+names.shift()
+console.log(names)
---
+++ Sourse/chapter05/video39/video39.js
@@ -0,0 +1,8 @@
+console.log("video 39");
+
+const myClass = ["an", "hi"];
+console.log(myClass, myClass.length);
+
+for (let i = 0; i < myClass.length; i++) {
+  console.log("i = ", i, "and value = ", myClass[i]);
+}
